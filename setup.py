536a1e1d541b5dbbd9785ca6d36c99f8ee4e284b
whoisprakhar44 <prakharshukla2505@gmail.com>
2024-03-14 19:57:59 +0530

    folder structure created

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = '-e.'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements = []
+
+    with open(file_path) as file_obj:
+        requirements=file_obj.readline()
+        requirements=[req.replace("\n", " ")for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    
+    return requirements
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="Prakhar",
+    author_email="prakharshukla2505@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
